f67b93f75062a3ba393f3437e8be249d9520223c
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-04 11:02:13 +0200

    Adding Bootstrap

--- header.php
+++ header.php
@@ -1,21 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Awsome Theme</title>
-        <!-- With wp_head() we call all the enqueue styles we created on the functions file -->
-        <?php wp_head(); ?>
-    </head>
-    <?php
-        if(is_front_page()) :
-            $awsome_classes = array('awsome-class', 'my-class');
-        else :
-            $awsome_classes = array('no-awsome-class');
-        endif;
-    ?>
-    <body <?php body_class($awsome_classes); ?>>
-        <!-- Calling the menu named primary on our header -->
-        <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
-        <img src="<?php header_image(); ?>" height="<?php echo get_custom_header() -> height; ?>" width="<?php echo get_custom_header() -> width; ?>" alt="">
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Awesome Theme</title>
+		<?php wp_head(); ?>
+	</head>
+	<?php 
+
+		if( is_front_page() ):
+			$awesome_classes = array( 'awesome-class', 'my-class' );
+		else:
+			$awesome_classes = array( 'no-awesome-class' );
+		endif;
+	?>
+	<body <?php body_class( $awesome_classes ); ?>>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<nav class="navbar navbar-default">
+					  <div class="container-fluid">
+					    <!-- Brand and toggle get grouped for better mobile display -->
+					    <div class="navbar-header">
+					      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+					      <a class="navbar-brand" href="#">Awesome Theme</a>
+					    </div>
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<?php 
+								wp_nav_menu(array(
+									'theme_location' => 'primary',
+									'container' => false,
+									'menu_class' => 'nav navbar-nav navbar-right'
+									)
+								);
+							?>
+						</div>
+					  </div><!-- /.container-fluid -->
+					</nav>
+				</div>
+			</div>
+			<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /> 
