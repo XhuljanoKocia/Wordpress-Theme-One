09923a93018a47894c2b69a639c0f3d562bc45a3
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-05 12:20:24 +0200

    Carousel

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
-        <footer>
-            <p>This is my footer</p>
-            <!-- Calling the menu named secondary on our footer -->
-            <?php wp_nav_menu(array('theme_location' => 'secondary')); ?>
-        </footer>
-        <?php wp_footer(); ?>
-    </body>
+	
+		<footer>
+			<p>This is my footer</p>
+			<?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+		</footer>
+	
+	</div><!-- .container -->
+	
+	<?php wp_footer(); ?>
+	
+	</body>
 </html>
--- page-home.php
+++ page-home.php
@@ -1,125 +1,156 @@
 <?php get_header(); ?>
 
 <div class="row">
-
-    <?php
-        //We show only the first post
-        // $lastBlog = new WP_Query('type=post&posts_per_page=1');
-
-        $args_cat = array(
-            'include' => '7, 8, 9',
-        );
-
-        $categories = get_categories($args_cat);
-        foreach($categories as $category):
-            $args = array(
-                'type' => 'post',
-                'posts_per_page' => 1,
-                'category__in' => $category->term_id, //This includes the category ids which will be shown
-                'category__not_in' => array(1), //This escludes the category ids which we don't want to show
-            );
-    
-            $lastBlog = new WP_Query($args);
-    
-            //We check if there are any posts 
-            if($lastBlog -> have_posts()) :
-                //We loop throught the posts 
-                while($lastBlog -> have_posts()) :
-                    //This will print all the posts, their titles and content as well as the time(date, month, hour) and category
-                    $lastBlog -> the_post();
-            ?>
-    
-            <div class="col-xs-12 col-sm-4">
-    
-                <?php get_template_part('content', 'featured'); ?>
-    
-            </div>
-                        
-            <?php
-                    endwhile;
-                endif;
-    
-                wp_reset_postdata();
-        endforeach;
-
-    ?>
-
+		
+		<div class="col-xs-12">
+		
+		<div id="awesome-carousel" class="carousel slide" data-ride="carousel">
+		
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+			  
+			<?php 
+			
+				$args_cat = array(
+					'include' => '6, 7, 8'
+				);
+				
+				$categories = get_categories($args_cat);
+				$count = 0;
+				$bullets = '';
+				foreach($categories as $category):
+					
+					$args = array( 
+						'type' => 'post',
+						'posts_per_page' => 1,
+						'category__in' => $category->term_id,
+						'category__not_in' => array( 1 ),
+					);
+					
+					$lastBlog = new WP_Query( $args ); 
+					
+					if( $lastBlog->have_posts() ):
+						
+						while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+							
+							<div class="item <?php if($count == 0): echo 'active'; endif; ?>">
+						      <?php the_post_thumbnail('full'); ?>
+						      <div class="carousel-caption">
+							      <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h1>' ); ?>
+	
+								  <small><?php the_category(' '); ?></small>
+						      </div>
+						    </div>
+						    
+						    <?php $bullets .= '<li data-target="#awesome-carousel" data-slide-to="'.$count.'" class="'; ?>
+						    <?php if($count == 0): $bullets .='active'; endif; ?>
+						    
+						    <?php  $bullets .= '"></li>'; ?>
+						
+						<?php endwhile;
+						
+					endif;
+					
+					wp_reset_postdata();
+				
+				$count++;
+					
+				endforeach;
+			
+			?>
+			
+			<!-- Indicators -->
+			  <ol class="carousel-indicators">
+			    <?php echo $bullets; ?>
+			  </ol>
+		    
+		  </div>
+		
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#awesome-carousel" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#awesome-carousel" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+		
+		</div>
+		
 </div>
 
 <div class="row">
+	
+	<div class="col-xs-12 col-sm-8">
 
-    <div class="col-xs-12 col-sm-8">
-
-        <?php
-            //We check if there are any posts 
-            if(have_posts()) :
-                //We loop throught the posts 
-                while(have_posts()) :
-                    //This will print all the posts, their titles and content as well as the time(date, month, hour) and category
-                    the_post();
-        ?>
-        <?php get_template_part('content', get_post_format()); ?>
-                    
-        <?php
-                endwhile;
-            endif;
-
-            //Print 2 other posts without repeating the first post, offset=1 skips the first post
-            //Using an array makes the code more clear and more readable since we might have more than 3 arguments
-/*            $args = array(
-                'type' => 'post',
-                'posts_per_page' => 2,
-                'offset' => 1
-            );
-
-            $lastBlog = new WP_Query($args);
-
-                //We check if there are any posts 
-                if($lastBlog -> have_posts()) :
-                    //We loop throught the posts 
-                    while($lastBlog -> have_posts()) :
-                        //This will print all the posts, their titles and content as well as the time(date, month, hour) and category
-                        $lastBlog -> the_post();
-            ?>
-            <?php get_template_part('content', get_post_format()); ?>
-                        
-            <?php
-                    endwhile;
-                endif;
-
-                wp_reset_postdata();
+		<?php 
+		
+		if( have_posts() ):
+			
+			while( have_posts() ): the_post(); ?>
+				
+				<?php get_template_part('content',get_post_format()); ?>
+			
+			<?php endwhile;
+			
+		endif;
+		
+		//PRINT OTHER 2 POSTS NOT THE FIRST ONE
+/*
+		$args = array(
+			'type' => 'post',
+			'posts_per_page' => 2,
+			'offset' => 1,
+		);
+		
+		$lastBlog = new WP_Query($args);
+			
+		if( $lastBlog->have_posts() ):
+			
+			while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+				
+				<?php get_template_part('content',get_post_format()); ?>
+			
+			<?php endwhile;
+			
+		endif;
+		
+		wp_reset_postdata();
 */
-            ?>
-
-            <!-- <hr> -->
-
-            <?php
-                //Print only tutorial category posts, we get them with category page id or we can use category_name=tutorials if the name is not going to change
-/*            $lastBlog = new WP_Query('type=post&posts_per_page=-1&cat=9');
-
-            //We check if there are any posts 
-            if($lastBlog -> have_posts()) :
-                //We loop throught the posts 
-                while($lastBlog -> have_posts()) :
-                    //This will print all the posts, their titles and content as well as the time(date, month, hour) and category
-                    $lastBlog -> the_post();
-            ?>
-            <?php get_template_part('content', get_post_format()); ?>
-                        
-            <?php
-                    endwhile;
-                endif;
-
-                wp_reset_postdata();
+				
+		?>
+		
+		<!-- <hr> -->
+		
+		<?php
+			
+		//PRINT ONLY TUTORIALS
+/*
+		$lastBlog = new WP_Query('type=post&posts_per_page=-1&category_name=news');
+			
+		if( $lastBlog->have_posts() ):
+			
+			while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+				
+				<?php get_template_part('content',get_post_format()); ?>
+			
+			<?php endwhile;
+			
+		endif;
+		
+		wp_reset_postdata();
 */
-            ?>
-
-    </div>
-
-<div class="col-xs-12 col-sm-4">
-    <?php get_sidebar(); ?>
+				
+		?>
+	
+	</div>
+	
+	<div class="col-xs-12 col-sm-4">
+		<?php get_sidebar(); ?>
+	</div>
+	
 </div>
 
-</div>
-    
 <?php get_footer(); ?>
