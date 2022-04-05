ac357aa776d66d3127529ba5be0c02fdab0a24e0
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-05 12:07:32 +0200

    Filter the WP_Query with categories

--- page-home.php
+++ page-home.php
@@ -1,26 +1,47 @@
 <?php get_header(); ?>
 
-<div class="row"></div>
+<div class="row">
 
-<div class="col-xs-12">
     <?php
         //We show only the first post
-        $lastBlog = new WP_Query('type=post&posts_per_page=1');
+        // $lastBlog = new WP_Query('type=post&posts_per_page=1');
 
-        //We check if there are any posts 
-        if($lastBlog -> have_posts()) :
-            //We loop throught the posts 
-            while($lastBlog -> have_posts()) :
-                //This will print all the posts, their titles and content as well as the time(date, month, hour) and category
-                $lastBlog -> the_post();
-    ?>
-    <?php get_template_part('content', get_post_format()); ?>
-                
-    <?php
-            endwhile;
-        endif;
+        $args_cat = array(
+            'include' => '7, 8, 9',
+        );
 
-        wp_reset_postdata();
+        $categories = get_categories($args_cat);
+        foreach($categories as $category):
+            $args = array(
+                'type' => 'post',
+                'posts_per_page' => 1,
+                'category__in' => $category->term_id, //This includes the category ids which will be shown
+                'category__not_in' => array(1), //This escludes the category ids which we don't want to show
+            );
+    
+            $lastBlog = new WP_Query($args);
+    
+            //We check if there are any posts 
+            if($lastBlog -> have_posts()) :
+                //We loop throught the posts 
+                while($lastBlog -> have_posts()) :
+                    //This will print all the posts, their titles and content as well as the time(date, month, hour) and category
+                    $lastBlog -> the_post();
+            ?>
+    
+            <div class="col-xs-12 col-sm-4">
+    
+                <?php get_template_part('content', 'featured'); ?>
+    
+            </div>
+                        
+            <?php
+                    endwhile;
+                endif;
+    
+                wp_reset_postdata();
+        endforeach;
+
     ?>
 
 </div>
@@ -43,7 +64,7 @@
 
             //Print 2 other posts without repeating the first post, offset=1 skips the first post
             //Using an array makes the code more clear and more readable since we might have more than 3 arguments
-            $args = array(
+/*            $args = array(
                 'type' => 'post',
                 'posts_per_page' => 2,
                 'offset' => 1
@@ -65,13 +86,14 @@
                 endif;
 
                 wp_reset_postdata();
+*/
             ?>
 
-            <hr>
+            <!-- <hr> -->
 
             <?php
                 //Print only tutorial category posts, we get them with category page id or we can use category_name=tutorials if the name is not going to change
-            $lastBlog = new WP_Query('type=post&posts_per_page=-1&cat=9');
+/*            $lastBlog = new WP_Query('type=post&posts_per_page=-1&cat=9');
 
             //We check if there are any posts 
             if($lastBlog -> have_posts()) :
@@ -87,6 +109,7 @@
                 endif;
 
                 wp_reset_postdata();
+*/
             ?>
 
     </div>
