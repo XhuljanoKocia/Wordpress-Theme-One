64af96f8f2c2bbd0df3cbcfdbdaed56ee62a2b24
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-06 11:26:21 +0200

    Fixing Walker Bug

--- inc/walker.php
+++ inc/walker.php
@@ -23,17 +23,17 @@
 
     class Walker_Nav_primary extends Walker_Nav_Menu {
         
-        function start_lvl( &$output, $depth ){ //ul
+        function start_lvl( &$output, $depth = 0, $args = null ){ //ul
             $indent = str_repeat("\t",$depth);
             $submenu = ($depth > 0) ? ' sub-menu' : '';
             $output .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
         }
-
-        function start_el(){ //li a span
+/*
+        function start_el( &$output, $item, $depth = 0 ){ //li a span
 
         }
 
-/*
+
         function end_el(){ //closing li a span
 
         }
