b2b1a18769a2162dffac4da09936cff62bc1f118
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-04-07 10:02:34 +0200

    How to create Custom Taxonomies - Part 1

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ function awesome_script_enqueue() {
 
 add_action( 'wp_enqueue_scripts', 'awesome_script_enqueue');
 
+
 /*
 	==========================================
 	 Activate menus
@@ -121,10 +122,44 @@ function awesome_custom_post_type (){
 			'thumbnail',
 			'revisions',
 		),
-		'taxonomies' => array('category', 'post_tag'),
+		// 'taxonomies' => array('category', 'post_tag'),
 		'menu_position' => 5,
 		'exclude_from_search' => false
 	);
 	register_post_type('portfolio',$args);
 }
 add_action('init','awesome_custom_post_type');
+
+function awesome_custom_taxonomies() {
+	
+	//add new taxonomy hierarchical
+	$labels = array(
+		'name' => 'Types',
+		'singular_name' => 'Type',
+		'search_items' => 'Search Types',
+		'all_items' => 'All Types',
+		'parent_item' => 'Parent Type',
+		'parent_item_colon' => 'Parent Type:',
+		'edit_item' => 'Edit Type',
+		'update_item' => 'Update Type',
+		'add_new_item' => 'Add New Work Type',
+		'new_item_name' => 'New Type Name',
+		'menu_name' => 'Types'
+	);
+	
+	$args = array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => array( 'slug' => 'type' )
+	);
+	
+	register_taxonomy('type', array('portfolio'), $args);
+	
+	//add new taxonomy NOT hierarchical
+	
+}
+
+add_action( 'init' , 'awesome_custom_taxonomies' );
